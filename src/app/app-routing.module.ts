a3cc116ce20bce99f6e99f280889998d5d16be83
Franco Jesus Mamani Pozo <mamanipozofrancojesus@gmail.com>
2023-06-19 22:15:30 -0400

    JOY-0001: Creacion de componente client

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientComponent } from './components/client/client.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'clientes',
+  component: ClientComponent
+}, {
+  path: '',
+  redirectTo: 'clientes',
+  pathMatch: 'full'
+}, {
+  path: '**',
+  redirectTo: 'clientes',
+  pathMatch: 'full'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientComponent } from './components/client/client.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent
   ],
   imports: [
     BrowserModule,
